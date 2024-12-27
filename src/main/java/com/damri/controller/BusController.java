2f3bbdc18fbfb72d97c5aa1a759e9f29bceb3316
Mahira Irgiani <mahirairgn@gmail.com>
2024-12-27 05:02:03 +0000

    fix some code

--- src/main/java/com/damri/controller/BusController.java
+++ src/main/java/com/damri/controller/BusController.java
@@ -5,22 +5,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
 
 import com.damri.model.Bus;
 import com.damri.service.busService;
 
+@Controller
+@RequestMapping("/bus")
 public class BusController {
 
     @Autowired
     private busService busService;
 
-    @GetMapping("/bus/viewall")
+    @GetMapping("/viewall")
     public String listProyek(Model model) {
-
         List<Bus> listBus = busService.getAllBus();
-    
         model.addAttribute("listBus", listBus);
-
-        return "viewall-bus";
+        return "viewall-bus"; // Return Thymeleaf template
     }
 }
